11c68ceb98b119803eca1833a53cc3f7b428d62d
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-14 09:08:19 +0300

    Day 3 start work

--- app/auth_admin/forms.py
+++ app/auth_admin/forms.py
@@ -5,18 +5,18 @@ from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email address', validators=[Required(), Email()])
+    email = StringField('Enter a valid email address', validators=[Required(), Email()])
     username = StringField('Enter your username', validators=[Required()])
-    password = PasswordField('Passwords', validators=[Required(), EqualTo('password_confirm', message='Password must match')])
-    password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message='Password must match')])
+    password_confirm = PasswordField('Confirm Password', validators=[Required()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, data_field):
         if User.query.filter_by(email = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('That username is already taken')
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('That username is already taken')
 
 
 class LoginForm(FlaskForm):
